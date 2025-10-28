7366f4eafe6a2a712c7003def8f796f5f5510f81
daqhi <eliabado29@gmail.com>
2025-10-28 21:35:10 +0800

    implemented ui recommendation from tester

--- frontend/src/pages/complainant/homepage.jsx
+++ frontend/src/pages/complainant/homepage.jsx
@@ -17,16 +17,15 @@ const CU_HomePage = () => {
 function NewsCards() {
     // Example news data
     const news = [
-        { title: "News Title 1", desc: "Brief description of the news article goes here.", img: "/images/news.jpg" },
-        { title: "News Title 2", desc: "Brief description of the news article goes here.", img: "/images/news.jpg" },
-        { title: "News Title 3", desc: "Brief description of the news article goes here.", img: "/images/news.jpg" },
-        { title: "News Title 4", desc: "Brief description of the news article goes here.", img: "/images/news.jpg" },
-        { title: "News Title 5", desc: "Brief description of the news article goes here.", img: "/images/news.jpg" },
+        { title: "Desinyo sa mga Local Artists nga Inspired sa 20th Century Filipino Arists, Gi-showcase sa Ternocon 2025 Exhibit", desc: "10-28-2025", img: "/images/news/news.jpg" },
+        { title: "Get ready for the Iligan Paskohan Songwriwting Competition 2025!", desc: "10-28-2025", img: "/images/news/news5.jpg" },
+        { title: "LOOK: LGU Iligan City Named Regional Winner in TESDA Kabalikat Awards 2025", desc: "10-28-2025", img: "/images/news/news3.jpg" },
+        { title: "LIGTAS UNDAS 2025!", desc: "10-28-2025", img: "/images/news/news4.jpg" },
+        { title: "Libuan ka mga Senior Citizens sa Iligan, Gi-selebrar sa Elderly Filipino Week", desc: "10-28-2025", img: "/images/news/news1.jpg" },
     ];
 
-    const [index, setIndex] = useState(0); // tracks the starting index
+    const [index, setIndex] = useState(0);
 
-    // show 3 cards at a time
     const visibleCount = 3;
 
     // handle navigation with looping
@@ -38,14 +37,13 @@ function NewsCards() {
         setIndex((prev) => (prev - 1 + news.length) % news.length);
     };
 
-    // compute visible items
     const visibleNews = [];
     for (let i = 0; i < visibleCount; i++) {
         visibleNews.push(news[(index + i) % news.length]);
     }
 
     return (
-        <div className="bg-white mb-10 text-black mx-20">
+        <div className="bg-white mb-10 text-black mx-10 my-10">
             {/* Header */}
             <div className="flex flex-row justify-between items-center">
                 <div>
@@ -67,15 +65,15 @@ function NewsCards() {
             </div>
 
             {/* News Cards */}
-            <div className="flex flex-row gap-20 items-center justify-center mt-8 transition-transform duration-500 ease-in-out">
+            <div className="flex flex-row gap-20 items-center justify-center justify-between mt-8 transition-transform duration-500 ease-in-out">
                 {visibleNews.map((item, i) => (
                   <div key={i} className="hover:bg-white transform transition duration-300 hover:scale-105" >
                         <div
-                            className={`bg-[url('${item.img}')] bg-cover bg-center h-80 w-80 rounded-2xl shadow-lg`}
+                            className={`bg-[url('${item.img}')] bg-cover bg-center h-96 w-96 rounded-2xl shadow-lg`}
                         ></div>
                         <div className="border-l-4 border-yellow-400 pl-2 mt-3">
-                            <h1 className="font-bold text-black text-lg">{item.title}</h1>
-                            <p className="text-black text-sm w-70 leading-tight">{item.desc}</p>
+                            <h1 className="font-bold text-black text-xl overflow-auto w-80">{item.title}</h1>
+                            <p className="text-gray-500 text-base w-70 mt-3">{item.desc}</p>
                         </div>
                   </div>
                 ))}
@@ -87,37 +85,37 @@ function NewsCards() {
 
 function CityPrograms(){
     return (
-        <div className='bg-gray-100 h-80 px-20'>
-            <h1 className='font-bold text-black text-3xl leading-tight pt-10'>City Programs</h1>
+        <div className='flex flex-col bg-gray-100 h-80 px-10 justify-center'>
+            <h1 className='font-bold text-black text-3xl leading-tight'>City Programs</h1>
             <p className='text-gray-600 leading-tight text-sm'>Learn more about the city's major plans and programs</p>
 
             {/* City programs cards */}
-            <div className='flex flex-row items-center mt-8 mb-5 gap-10 justify-center'>
+            <div className='flex flex-row items-center mt-8 mb-5 gap-10 justify-center justify-between'>
 
-                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/people.jpeg')] bg-cover bg-center bg-blend-overlay bg-red-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-60 h-32">
+                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/cityprograms/people.jpeg')] bg-cover bg-center bg-blend-overlay bg-red-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-80 h-40">
                     <h1 className="text-2xl text-white font-extrabold [text-shadow:_2px_2px_4px_rgba(0,0,0,0.7)] leading-none  mb-3">Free Wifi for All</h1>
                     <button className="bg-white border border-white rounded-md px-4 py-1 text-sm text-red-400 font-medium hover:bg-white transform transition duration-300 hover:scale-105">
                         Learn More
                     </button>
                 </div>
 
-                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/iligan-port.jpg')] bg-cover bg-center bg-blend-overlay bg-green-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-60 h-32">
+                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/cityprograms/iligan-port.jpg')] bg-cover bg-center bg-blend-overlay bg-green-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-80 h-40">
                     <h1 className="text-2xl text-white font-extrabold [text-shadow:_2px_2px_4px_rgba(0,0,0,0.7)] leading-none  mb-3">Investments and Promotions</h1>
                     <button className="bg-white border border-white rounded-md px-4 py-1 text-sm text-green-400 font-medium hover:bg-white transform transition duration-300 hover:scale-105">
                         Learn More
                     </button>
                 </div>
 
-                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/vaccine.jpg')] bg-cover bg-center bg-blend-overlay bg-blue-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-60 h-32">
+                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/cityprograms/vaccine.jpg')] bg-cover bg-center bg-blend-overlay bg-blue-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-80 h-40">
                     <h1 className="text-2xl text-white font-extrabold [text-shadow:_2px_2px_4px_rgba(0,0,0,0.7)] leading-none  mb-3">Covid-19 Vaccination</h1>
                     <button className="bg-white border border-white rounded-md px-4 py-1 text-sm text-blue-400 font-medium hover:bg-white transform transition duration-300 hover:scale-105">
                         Learn More
                     </button>
                 </div>
 
-                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/iligan-port.jpg')] bg-cover bg-center bg-blend-overlay bg-purple-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-60 h-32">
+                <div className="relative bg-[linear-gradient(rgba(255,255,255,0.1),rgba(255,255,255,0.1)),url('/images/cityprograms/iligan-port.jpg')] bg-cover bg-center bg-blend-overlay bg-purple-300/90 rounded-2xl shadow-lg flex flex-col items-center justify-center text-center text-white w-80 h-40">
                     <h1 className="text-2xl text-white font-extrabold [text-shadow:_2px_2px_4px_rgba(0,0,0,0.7)] leading-none  mb-3">City Wide Transformation</h1>
-                    <button className="bg-white border border-white rounded-md px-4 py-1 text-sm text-blue-400 font-medium hover:bg-white transform transition duration-300 hover:scale-105">
+                    <button className="bg-white border border-white rounded-md px-4 py-1 text-sm text-purple-400 font-medium hover:bg-white transform transition duration-300 hover:scale-105">
                         Learn More
                     </button>
                 </div>
@@ -129,7 +127,7 @@ function CityPrograms(){
 
 function Feedback() {
     return(
-        <div className='bg-gradient-to-r from-[#378591] to-[#1b4f57] text-white mx-20 my-10 p-10 flex justify-between items-center rounded-2xl'>
+        <div className='bg-gradient-to-r from-[#378591] to-[#1b4f57] text-white mx-10 my-10 px-20 py-10 flex justify-between items-center rounded-2xl'>
             <div>
                 <h1 className='text-2xl font-extrabold'>We would like to hear from you</h1>
                 <p>Send us your feedback, comments and suggestions to help improve our services</p>
