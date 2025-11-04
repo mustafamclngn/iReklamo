17436b9cf505bbb4afbf65743c30bcf398621495
Elia Fel P. Bado <eliafel.bado@g.msuiit.edu.ph>
2025-11-04 22:38:10 +0800

    IGC-12: created a rough outline for the brgy capt. dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
     return (
     <Routes>
         {/* Home route (Default) */}
-        <Route path="/" element={<ComplainantHomeLayout />}>
+        <Route path="/" element={<BC_DashboardPage />}>
           <Route index element={<Navigate to="home" replace />} />
           <Route path="home" element={<CU_HomePage />} />
         </Route>
--- frontend/src/pages/brgyCap/dashboardpage.jsx
+++ frontend/src/pages/brgyCap/dashboardpage.jsx
@@ -2,7 +2,43 @@ import React from 'react';
 
 const BC_DashboardPage = () => {
   return (
-    <p class="text-4xl font-bold text-center mt-16"> This is the Barangay Captain Dashboard Page!</p>
+    <div>
+        <div className=''>
+            <h1>Welcome to your dashboard, Kapitan!</h1>
+            <p>Himuon taning dasdfasdfasd</p>
+            <div>
+                <p>Barangay Captian:</p>
+                <p>Jose P. Riizzal</p>
+            </div>
+        </div>
+
+        <div>
+            {/* right */}
+            <div>
+                <div>
+                    CARDS
+                </div>
+                <div>
+                    <div>
+                        Stats for case type
+                    </div>
+                    <div>
+                        PIE CHART
+                    </div>
+                </div>
+            </div>
+
+            {/* LEFT */}
+            <div>
+                <div>
+                    urgent
+                </div>
+                <div>
+                    officils
+                </div>
+            </div>
+        </div>
+    </div>
   );
 }
 
