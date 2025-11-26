cc489119db13944a349abda83bc1fa9f81a2b823
fallerfare <joshualouise.villanueva@g.msuiit.edu.ph>
2025-11-27 00:44:39 +0800

    IGC-101: Copleted backend for Logging out, modularized the revokal of tokens

--- frontend/src/pages/brgyCap/accountpage.jsx
+++ frontend/src/pages/brgyCap/accountpage.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import useLogOut from '../../hooks/useLogout';
+import { useNavigate, Link } from 'react-router-dom';
 
 const BC_AccountPage = () => {
+
+  const logout = useLogOut();
+  const navigate = useNavigate();
+
+  const signout = async () => {
+    await logout();
+    navigate('/home');
+  }
+
+
   return (
-    <p class="text-4xl font-bold text-center mt-16"> This is the Barangay Captain Account Page!</p>
+        <button 
+        onClick={() => signout()}
+        className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors font-medium"
+    >
+        Log Out
+    </button>
   );
 }
 
--- frontend/src/pages/brgyOff/accountpage.jsx
+++ frontend/src/pages/brgyOff/accountpage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const BO_AccountPage = () => {
+    
+  
   return (
-    <p class="text-4xl font-bold text-center mt-16"> This is the Barangay Official Account Page!</p>
-  );
-}
+    <button 
+        onClick={() => console.log("Log Out")}
+        className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors font-medium"
+    >
+        Log Out
+    </button>
+    );
+  }
 
 export default BO_AccountPage;
