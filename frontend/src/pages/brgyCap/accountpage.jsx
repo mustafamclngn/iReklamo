24dbb5f4fd8a519b8ab6add66b4f7c327e23ceae
fallerfare <joshualouise.villanueva@g.msuiit.edu.ph>
2025-11-27 00:55:19 +0800

    IGC-101: Confirm Logout Modals

--- frontend/src/pages/brgyCap/accountpage.jsx
+++ frontend/src/pages/brgyCap/accountpage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useLogOut from '../../hooks/useLogout';
 import { useNavigate, Link } from 'react-router-dom';
 
@@ -7,6 +7,8 @@ const BC_AccountPage = () => {
   const logout = useLogOut();
   const navigate = useNavigate();
 
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+
   const signout = async () => {
     await logout();
     navigate('/home');
@@ -14,12 +16,22 @@ const BC_AccountPage = () => {
 
 
   return (
-        <button 
-        onClick={() => signout()}
-        className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors font-medium"
-    >
-        Log Out
-    </button>
+    <>
+      <button
+          onClick={() => setIsConfirmOpen(true)}
+          className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors font-medium"
+      >
+          Log Out
+      </button>
+      
+      <LogOutModal 
+        isOpen={isConfirmOpen} 
+        onClose={() => setIsConfirmOpen(false)}
+        onConfirm={() => signout()}
+        >
+      </LogOutModal>
+    
+    </>
   );
 }
 
