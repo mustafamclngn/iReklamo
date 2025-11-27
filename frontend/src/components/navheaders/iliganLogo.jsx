e905a980150e21b78f535b0cc4c9dc79f65df91a
fallerfare <joshualouise.villanueva@g.msuiit.edu.ph>
2025-11-27 12:00:25 +0800

    IGC-98:Refactored login page to be similar to the official iligan gov

--- frontend/src/components/navheaders/iliganLogo.jsx
+++ frontend/src/components/navheaders/iliganLogo.jsx
@@ -2,23 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // Iligan Logo Component
-const IliganLogo = () => {
-  const links = ["Dashboard", "Complaints", "Officials", "Reports", "Account"];
-
+const IliganLogo = ({ scale = 1, mode = "default"}) => {
   return (
-        <Link to="/home" className="flex items-center gap-4">
-          <img
-            src="https://iligan.gov.ph/assets/img/iligancity-welcome-logo.png"
-            alt="Iligan City Logo"
-            className="h-14"
-          />
-          <img
-            src="https://iligan.gov.ph/assets/img/logo-website.png"
-            alt="Iligan City Seal"
-            className="h-14"
-          />
-        </Link>
+    <Link 
+      to="/home"
+      className={`flex items-center ${mode === "default" ? "gap-4" : "gap-1"}`}
+      style={{ zoom: scale }}  
+    >
+      {mode === "login" && (
+        <img
+          src="https://iligan.gov.ph/assets/img/logo-website.png"
+          alt="Iligan City Seal"
+          className="h-10"
+        />
+      )}
+
+      <img
+        src="https://iligan.gov.ph/assets/img/iligancity-welcome-logo.png"
+        alt="Iligan City Logo"
+        className={mode === "login" ? "h-14" : "h-14"}
+      />
+
+      {mode === "default" && (
+        <img
+          src="https://iligan.gov.ph/assets/img/logo-website.png"
+          alt="Iligan City Seal"
+          className="h-14"
+        />
+      )}
+    </Link>
   );
 };
+
 
 export default IliganLogo;
